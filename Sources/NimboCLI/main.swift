6db9c8d3339540f0393aa5c560f1477c95eb6665
gscalzo <gscalzo@users.noreply.github.com>
2025-09-08 18:32:49 +0100

    fix

--- Sources/NimboCLI/main.swift
+++ Sources/NimboCLI/main.swift
@@ -7,22 +7,24 @@ private enum AnsiColor: String {
     case green = "\u{001B}[32m"
 }
 
-private func color(_ text: String, in color: AnsiColor) -> String {
+private func display(_ text: String, in color: AnsiColor) -> String {
     return "\(color.rawValue)\(text)\(AnsiColor.reset.rawValue)"
 }
 
 @discardableResult
 private func input() -> String? {
-    print("\(color("You", in: .blue)): ", terminator: "")  
+    print("\(display("You", in: .blue)): ", terminator: "")  
     fflush(stdout)
     return readLine()?.trimmingCharacters(in: .whitespacesAndNewlines)
 }
 
 private func runLoop() {
-    while let line = input() {    
-        let input = line          
-        
-        print("\(color("Nimbo", in: .green)): \(input)")
+    print("\nChat with Nimbo (use 'ctrl-c' to quit)\n")
+
+    while let line = input() {
+        let input = line
+
+        print("\(display("Nimbo", in: .green)): \(input)")
     }
 }
 
